24fb61a16f9a4978dd444ebe0af088685b879e46
Maureen Kriska Martel <martel.maureen@orange.fr>
2022-07-28 15:02:17 +0200

    Affichage du numéro de commande

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,3 +1,24 @@
 //----------------------------------------------------------------------------------//
 //------------------------ Affichage du numéro de commande -------------------------//
 //----------------------------------------------------------------------------------//
+
+// --- Rechercher l'id de la commande depuis l'URL
+function getUrlParamsOrderId() {
+  const urlRequest = window.location.search         
+  const urlParameters = new URLSearchParams(urlRequest) 
+  const orderId = urlParameters.get('order-id')        
+  return orderId
+}
+
+const orderId = getUrlParamsOrderId()
+
+// --- Fonction pour indiquer où afficher le numéro de commande
+function orderNumberDisplay() {
+  document.getElementById('orderId').innerText = orderId
+}
+orderNumberDisplay()
+
+
+
+
+
